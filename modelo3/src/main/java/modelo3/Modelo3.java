6eaab2b1e6178f4f81fab70c6500e3f31eeb93cd
Nicolas Speratti <nicolasasperatti@gmail.com>
2025-07-29 15:20:20 -0300

    creacion del proyecto de modelo 3 infracciones

---
+++ modelo3/src/main/java/modelo3/Modelo3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package modelo3;
+
+/**
+ *
+ * @author kitty
+ */
+public class Modelo3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
