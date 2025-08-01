c2eb076d780933480b5d2907388416b9e7cf3363
Nicolas Speratti <nicolasasperatti@gmail.com>
2025-08-01 17:58:57 -0300

    creando proyecto y clases vacias

---
+++ modelo5/src/main/java/modelo5/Modelo5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package modelo5;
+
+/**
+ *
+ * @author kitty
+ */
+public class Modelo5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
