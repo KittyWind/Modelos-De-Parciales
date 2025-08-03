6b8fd011b6d4759cb23db9e01909ae19b1aeef24
Nicolas Speratti <nicolasasperatti@gmail.com>
2025-08-03 03:43:00 -0300

    Completando NaveCruiser.java y Modelo5.java del proyecto modelo5

--- modelo5/src/main/java/modelo5/Modelo5.java
+++ modelo5/src/main/java/modelo5/Modelo5.java
@@ -13,8 +13,14 @@ public class Modelo5 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         EstacionEspacial estacion = new EstacionEspacial();
-        NaveCargo naveCargo = new NaveCargo("Tierra", 5, new Carga("Carga General", 100));
-        estacion.estacionarNave(naveCargo);
+        Nave nave1 = new NaveCargo("Neptuno", 21, new Carga("Baterias recargables", 120));
+        Nave nave2 = new NaveCruiser("Mercurio",8,0);
+        Nave nave3 = new NaveCargo("Marte",15,new Carga("Sustancias Cosmicas",90));
+        Nave nave4 = new NaveCruiser("Urano",48,12);
+        estacion.estacionarNave(nave1);
+        estacion.estacionarNave(nave2);
+        estacion.estacionarNave(nave3);
+        estacion.estacionarNave(nave4);
         estacion.mostrarManifiestos();
     }
 }
--- modelo5/src/main/java/modelo5/NaveCruiser.java
+++ modelo5/src/main/java/modelo5/NaveCruiser.java
@@ -9,6 +9,21 @@ package modelo5;
  *
  * @author kitty
  */
-public class NaveCruiser {
+public class NaveCruiser extends Nave {
+    private int pasajeros;
 
+    public NaveCruiser(String planetaOrigen, int tripulacion, int pasajeros) {
+        super(planetaOrigen, tripulacion);
+        this.pasajeros = pasajeros;
+    }
+
+    @Override
+    public boolean esApta() {
+        return this.pasajeros > 0;
+    }
+
+    @Override
+    public String devolverManifiesto() {
+        return "\nporcentaje de Pasajeros: " + ((double) pasajeros / (this.getTripulacion() + pasajeros)) * 100;
+    }
 }
