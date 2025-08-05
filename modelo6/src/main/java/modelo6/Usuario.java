ef49362642bb34968a8579cc26bbba1ea928ea83
Nicolas Speratti <nicolasasperatti@gmail.com>
2025-08-04 23:53:18 -0300

    llenando clase Usuario.java  con lo minimo

--- modelo6/src/main/java/modelo6/Usuario.java
+++ modelo6/src/main/java/modelo6/Usuario.java
@@ -1,4 +1,29 @@
 package modelo6;
 
 public class Usuario {
+    private String ID;
+    private String nombre;
+    private String apellido;
+    private Domicilio domicilio;
+
+    public Usuario(String ID, String nombre, String apellido, Domicilio domicilio) {
+        this.ID = ID;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.domicilio = domicilio;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "ID='" + ID + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", domicilio=" + domicilio +
+                '}';
+    }
+
+    public Domicilio getDomicilio() {
+        return domicilio;
+    }
 }
