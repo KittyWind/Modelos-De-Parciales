7deba65c86557e6c3668e9b9a7b9a5e6589a0cd9
Nicolas Speratti <nicolasasperatti@gmail.com>
2025-08-05 00:10:10 -0300

    llenando clase TodoLibre.java  con lo minimo

--- modelo6/src/main/java/modelo6/TodoLibre.java
+++ modelo6/src/main/java/modelo6/TodoLibre.java
@@ -1,5 +1,33 @@
 package modelo6;
 
-public class TodoLibre {
+import java.util.ArrayList;
 
+public class TodoLibre {
+    private ArrayList<Usuario> usuarios;
+    private ArrayList<Orden> ordenes;
+
+    public TodoLibre(){
+        this.ordenes = new ArrayList<>();
+        this.usuarios = new ArrayList<>();
+    }
+
+    public void aniadirOrden(Orden orden){
+        if (orden != null){
+            this.ordenes.add(orden);
+        }
+    }
+
+    public void aniadirUsuario(Usuario usuario){
+        if(usuario != null){
+            this.usuarios.add(usuario);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "TodoLibre{" +
+                "usuarios=" + usuarios +
+                ", ordenes=" + ordenes +
+                '}';
+    }
 }
