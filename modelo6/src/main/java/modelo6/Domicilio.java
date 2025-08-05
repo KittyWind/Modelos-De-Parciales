2e9f0a381bce33675149fd528446519e17568282
Nicolas Speratti <nicolasasperatti@gmail.com>
2025-08-04 23:00:06 -0300

    llenando clase Domicilio.java con lo minimo

--- modelo6/src/main/java/modelo6/Domicilio.java
+++ modelo6/src/main/java/modelo6/Domicilio.java
@@ -1,4 +1,22 @@
 package modelo6;
 
 public class Domicilio {
+    private String calle;
+    private int altura;
+    private int comuna;
+
+    public Domicilio(String calle, int altura, int comuna) {
+        this.calle = calle;
+        this.altura = altura;
+        this.comuna = comuna;
+    }
+
+    @Override
+    public String toString() {
+        return "Domicilio{" +
+                "calle='" + calle + '\'' +
+                ", altura=" + altura +
+                ", comuna=" + comuna +
+                '}';
+    }
 }
