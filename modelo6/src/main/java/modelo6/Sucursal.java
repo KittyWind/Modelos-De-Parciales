db653ced0deb9fb3d07095227af338c4792e2465
Nicolas Speratti <nicolasasperatti@gmail.com>
2025-08-04 23:03:30 -0300

    llenando clase Sucursal.java  con lo minimo

--- modelo6/src/main/java/modelo6/Sucursal.java
+++ modelo6/src/main/java/modelo6/Sucursal.java
@@ -1,4 +1,23 @@
 package modelo6;
 
 public class Sucursal {
+    private String nombre;
+    private Domicilio domicilio;
+
+    public Sucursal(String nombre, Domicilio domicilio) {
+        this.nombre = nombre;
+        this.domicilio = domicilio;
+    }
+
+    @Override
+    public String toString() {
+        return "Sucursal{" +
+                "nombre='" + nombre + '\'' +
+                ", domicilio=" + domicilio +
+                '}';
+    }
+
+    public Domicilio getDomicilio() {
+        return domicilio;
+    }
 }
